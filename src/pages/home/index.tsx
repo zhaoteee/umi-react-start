02cd7cb7a42bc1480813e8dbd377c7bb9afebad9
zhaote <zhaote555@gmail.com>
2021-06-07 15:20:30 +0800

    upd:首页添加完善

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,27 @@
 // import styles from './index.less';
 import React, { useState } from 'react';
+import goodsList from './testList';
+
+import styles from './index.less';
+
 
 export default function IndexPage() {
   const [isLoading, setIsLoading] = useState(false)
+  console.log(goodsList)
   return (
     <div>
       <p>商品列表</p>
-      <div className='goodsList'>
-        <div className='goodsItem'>
-        </div>
+      <div className={styles.goodsList}>
+        { goodsList.map((item, idx) => {
+          return <div key={idx} className={styles.product}>
+            <div>
+              <img
+                style={{maxWidth: '100%', maxHeight: '210px'}}
+                src={item.listPicUrl} alt="" />
+            </div>
+            <p>{item.name}</p>
+          </div>
+        }) }
       </div>
     </div>
   );
