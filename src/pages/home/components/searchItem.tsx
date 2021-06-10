e6786da8a912db26879a006894bb23559ba130a2
zhaote <zhaote555@gmail.com>
2021-06-10 09:19:55 +0800

    upd:处理eslint报错

--- src/pages/home/components/searchItem.tsx
+++ src/pages/home/components/searchItem.tsx
@@ -1,7 +1,12 @@
 // import styles from './index.less';
 import React, { useState } from 'react';
-import { PlusOutlined, DownOutlined, UpOutlined, CloseSquareFilled } from '@ant-design/icons';
-import { cloneDeep } from 'lodash'
+import {
+  PlusOutlined,
+  DownOutlined,
+  UpOutlined,
+  CloseSquareFilled,
+} from '@ant-design/icons';
+import { cloneDeep } from 'lodash';
 
 import styles from './searchItem.less';
 
@@ -9,99 +14,161 @@ export type OptionsItemType = {
   id: number;
   name: string;
   selected?: boolean;
-}
+};
 
 export type SearchItemProps = {
   name: string;
   value: string;
-  options: OptionsItemType[],
-  onSelect: (name: string, value: string, selectedOptions: OptionsItemType[]) => void
-}
+  options: OptionsItemType[];
+  onSelect: (
+    name: string,
+    value: string,
+    selectedOptions: OptionsItemType[],
+  ) => void;
+};
 
 const Search: React.FC<SearchItemProps> = (props) => {
-  const { name, value, options, onSelect } = props
-  const [optionList, setOptionList] = useState(options.map(item => {
-    item.selected = false
-    return item
-  }))
-  const [isShowMore, setIsShowMore] = useState(false)
-  const [selectMore, setSelectMore] = useState(false)
+  const { name, value, options, onSelect } = props;
+  const [optionList, setOptionList] = useState(
+    options.map((item) => {
+      item.selected = false;
+      return item;
+    }),
+  );
+  const [isShowMore, setIsShowMore] = useState(false);
+  const [selectMore, setSelectMore] = useState(false);
   const showMore = () => {
-    setIsShowMore(!isShowMore)
-  }
+    setIsShowMore(!isShowMore);
+  };
   const selectPlus = () => {
-    setSelectMore(true)
-  }
+    setSelectMore(true);
+  };
   const cancelSelectMore = () => {
     // 取消多选状态 所有选项selected置为false 触发搜索
-    let list = cloneDeep(optionList)
-    setOptionList(list.map(i => {
-      i.selected = false
-      return i
-    }))
-    setSelectMore(false)
-    onSelect(name, value, optionList.filter(item => item.selected))
-  }
+    const list = cloneDeep(optionList);
+    setOptionList(
+      list.map((i) => {
+        i.selected = false;
+        return i;
+      }),
+    );
+    setSelectMore(false);
+    onSelect(
+      name,
+      value,
+      optionList.filter((item) => item.selected),
+    );
+  };
   const clickItem = (item: OptionsItemType) => {
-    let list = cloneDeep(optionList)
-    if (item.selected) { // 点击已选择的取消选中状态
-      let current = list.find(i => i.id === item.id)
-      current ? current.selected = false : ''
+    const list = cloneDeep(optionList);
+    if (item.selected) {
+      // 点击已选择的取消选中状态
+      const current = list.find((i) => i.id === item.id);
+      if (current) current.selected = false;
     } else {
       // 单选状态
       if (!selectMore) {
-          list.map(i => {
-            i.selected = i.id === item.id
-            return i
-          })
-        } else { // 多选状态 selected直接置为true
-          let current = list.find(i => i.id === item.id)
-          current ? current.selected = true : ''
-        }
+        list.map((i) => {
+          i.selected = i.id === item.id;
+          return i;
+        });
+        return;
+      }
+      // 多选状态 selected直接置为true
+      const current = list.find((i) => i.id === item.id);
+      if (current) current.selected = true;
     }
     // 单选时直接触发搜索
     if (!selectMore) {
-      onSelect(name, value, optionList.filter(item => item.selected))
+      onSelect(
+        name,
+        value,
+        optionList.filter((op) => op.selected),
+      );
     }
-    setOptionList(list)
-  }
+    setOptionList(list);
+  };
   const onConfirm = () => {
     // 如果无选中 点击确定不触发搜索
-    if (!optionList.find(i => i.selected)) return
-    onSelect(name, value, optionList.filter(item => item.selected))
-  }
+    if (!optionList.find((i) => i.selected)) return;
+    onSelect(
+      name,
+      value,
+      optionList.filter((item) => item.selected),
+    );
+  };
   return (
     <div className={styles.attr}>
       <div className={styles.attrKey}>{name}</div>
       <div className={styles.attrValues}>
-        <ul className={`${styles.collapse} ${isShowMore ? styles.more : ''} ${selectMore ? styles.selectMore : ''}`}>
-          {optionList.map(item => <li
-            className={`${item.selected ? styles.selected : ''}`}
-            onClick={() => clickItem(item)}
-            key={item.id}
-          >
-            <a>
-              {item.name}<CloseSquareFilled className={`${styles.removeIcon} ${item.selected ? styles.showRemoveIcon : ''}`} />
-            </a></li>)}
+        <ul
+          className={`${styles.collapse} ${isShowMore ? styles.more : ''} ${
+            selectMore ? styles.selectMore : ''
+          }`}
+        >
+          {optionList.map((item) => (
+            <li
+              className={`${item.selected ? styles.selected : ''}`}
+              onClick={() => clickItem(item)}
+              key={item.id}
+            >
+              <a>
+                {item.name}
+                <CloseSquareFilled
+                  className={`${styles.removeIcon} ${
+                    item.selected ? styles.showRemoveIcon : ''
+                  }`}
+                />
+              </a>
+            </li>
+          ))}
         </ul>
-        {selectMore ? <div className={styles.selectMoreBtns}>
-          <a
-            className={`${optionList.find(item => item.selected) ? styles.btnSelected : styles.btnDisable}`}
-            onClick={onConfirm}
-          >确定</a>
-          <a onClick={cancelSelectMore}>取消</a>
-        </div> : null
-        }
-        <div className={`${styles.options} ${selectMore ? styles.selectedMoreOptions : ''}`}>
-          {
-            !selectMore ? <span><a onClick={selectPlus}><PlusOutlined />多选</a>
+        {selectMore ? (
+          <div className={styles.selectMoreBtns}>
+            <a
+              className={`${
+                optionList.find((item) => item.selected)
+                  ? styles.btnSelected
+                  : styles.btnDisable
+              }`}
+              onClick={onConfirm}
+            >
+              确定
+            </a>
+            <a onClick={cancelSelectMore}>取消</a>
+          </div>
+        ) : null}
+        <div
+          className={`${styles.options} ${
+            selectMore ? styles.selectedMoreOptions : ''
+          }`}
+        >
+          {!selectMore ? (
+            <span>
+              <a onClick={selectPlus}>
+                <PlusOutlined />
+                多选
+              </a>
               <a onClick={showMore}>
-                {isShowMore ? <span>收起<UpOutlined /></span> : <span>更多<DownOutlined /></span>}
-              </a></span> : ''
-          }
+                {isShowMore ? (
+                  <span>
+                    收起
+                    <UpOutlined />
+                  </span>
+                ) : (
+                  <span>
+                    更多
+                    <DownOutlined />
+                  </span>
+                )}
+              </a>
+            </span>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     </div>
   );
-}
-export default Search
+};
+export default Search;
