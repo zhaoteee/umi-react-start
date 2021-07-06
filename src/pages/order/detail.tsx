9abe2b10b1d68f05eb96de50b1ac526c2d1cd4ce
cat2dang <563739600@qq.com>
2021-07-06 16:42:23 +0800

    描述

--- src/pages/order/detail.tsx
+++ src/pages/order/detail.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect } from 'react';
-import { Button } from 'antd';
+import { Button, Popconfirm, message } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
 import { connect, useDispatch } from '@@/plugin-dva/exports';
+import type { Loading } from '@@/plugin-dva/connect';
+import { history } from 'umi';
 import type { OrderModelState, DetailInfo } from '@/models/detail';
+import { cancelOrder } from '@/services/order';
 import DetailHeader from './components/detailHeaderItem';
 import OrderHeader from './components/headerItem';
 import OrderItem from './components/orderItem';
 import DetailTotal from './components/detailTotalItem';
+import VoucherModal from './components/voucherModal';
 
 type detailQuery = { id: string };
 type DetailProps = {
   detail: DetailInfo;
+  loading: boolean;
   location: {
     query: detailQuery;
   };
@@ -22,34 +27,57 @@ const headerColumns = [
   { text: '金额', col: 3 },
 ];
 const OrderDetail: React.FC<DetailProps> = (props) => {
+  const [isShow, setIsShow] = React.useState(false);
   const { detail } = props;
+  const { query } = props.location
   const dispatch = useDispatch();
-  useEffect(() => {
+  const getData = (q) => {
+    setIsShow(false);
     dispatch({
       type: 'detail/getData',
+      payload: q
     });
+  }
+  const handleCancelOrder = (id: string) => {
+    cancelOrder(id).then(() => {
+      getData(query);
+      message.success('取消成功');
+    })
+  }
+  useEffect(() => {
+    getData(query)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [query]);
   return (
     <PageContainer
-      footer={[
-        <Button key="3">取消订单</Button>,
-        <Button key="2" type="primary">
-          线下打款
-        </Button>,
-      ]}
+      loading={props.loading}
+      footer={(detail.orderStatus === 'PART_PAY' || detail.orderStatus === 'WAIT_PAY') ? [
+        <Popconfirm
+          title="确认取消订单吗?"
+          onConfirm={() => handleCancelOrder(detail.id)}
+          okText="是"
+          cancelText="否"
+          key="4"
+        >
+          <Button>取消订单</Button>
+        </Popconfirm>,
+        detail.orderStatus === 'PART_PAY' && <Button key="2" type="primary" onClick={() => setIsShow(true)}>线下打款</Button>,
+        detail.orderStatus === 'WAIT_PAY' && <Button key="3" type="primary" onClick={() => history.push(`/mall/cart/paying?orderId=${detail.id}`)}>支付</Button>
+      ] : []}
     >
       <div>
         <DetailHeader info={detail} />
         <OrderHeader headerColumns={headerColumns} />
         <OrderItem info={detail} />
         <DetailTotal info={detail} />
+        <VoucherModal isShow={isShow} id={detail.id} onHandleHide={() => setIsShow(false)} onHandleOK={() => getData(query)}/>
       </div>
     </PageContainer>
   );
 };
-export default connect(({ detail }: { detail: OrderModelState }) => {
+export default connect(({ detail, loading }: { detail: OrderModelState, loading: Loading }) => {
   return {
     detail: detail.detail,
+    loading: loading.effects['detail/getData']
   };
 })(OrderDetail);
