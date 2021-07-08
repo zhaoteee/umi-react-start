441e546bddb4bca6d343ae649ace7ea871ca6d0a
zhaote <zhaote555@gmail.com>
2021-07-08 14:19:41 +0800

    upd:修改购物车及订单icon

--- src/components/MallHeader/index.tsx
+++ src/components/MallHeader/index.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { Input } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
 import type { ConnectState, CartModelState } from '@/models/connect';
 import type { HomeQueryType } from '@/pages/home/index';
 import { connect, Link, history, useLocation } from 'umi';
 import type { Location } from 'umi';
 import logo from '@/assets/images/logo.png';
-import cartIcon from '@/assets/images/cart-icon.png';
-import orderIcon from '@/assets/images/order-icon.png';
 
 import styles from './index.less';
 
@@ -44,11 +43,19 @@ const MallHeader: React.FC<MallHeaderType> = (props) => {
       </div>
       <div className={styles.orderInfo}>
         <Link className={styles.mycart} to="/mall/cart">
-          <img src={cartIcon} alt="购物车" />
+          <svg t="1625724881103" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7769" width="200" height="200">
+            <path d="M352 832m-64 0a64 64 0 1 0 128 0 64 64 0 1 0-128 0Z" p-id="7770" fill="#FF0036"></path>
+            <path d="M800 832m-64 0a64 64 0 1 0 128 0 64 64 0 1 0-128 0Z" p-id="7771" fill="#FF0036"></path>
+            <path
+              d="M913.6 241.56a47.84 47.84 0 0 0-37.12-17.56H267.78l-12.26-69.56A32 32 0 0 0 224 128H96a32 32 0 0 0 0 64h101.16l91.32 517.56A32 32 0 0 0 320 736h512a32 32 0 0 0 0-64H346.84l-11.28-64h483.32A48.14 48.14 0 0 0 866 569.42l57.6-288a48 48 0 0 0-10-39.86z"
+              p-id="7772"
+              fill="#FF0036"
+            ></path>
+          </svg>
           购物车({cart.total})
         </Link>
         <Link className={styles.myorder} to="/mall/order">
-          <img src={orderIcon} alt="订单" />
+          <MenuOutlined style={{ fontSize: '14px', marginRight: '2px' }} />
           我的订单
         </Link>
       </div>
