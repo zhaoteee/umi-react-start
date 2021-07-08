a89ba1e138cdc6e90cbaf8cd8b55ffc5aec040d2
yaomaofeng <yaomaofeng@zcckj.com>
2021-07-08 13:37:56 +0800

    更改首图展示

--- src/pages/goodsInfo/info.d.ts
+++ src/pages/goodsInfo/info.d.ts
@@ -8,29 +8,17 @@ export type productAttributeDTOs = [
   {
     attributeName: string;
     attributeValue: string;
-    createDate: string;
   },
 ];
 export type InfoItem = {
   brandId: string;
   brandName: string;
-  categoryId: string;
-  centerProductId: string;
-  code: string;
-  createDate: string;
   id: string;
   invoicePrice: string;
-  marketPrice: number;
-  salePrice: number;
-  salesNum: number;
   stock: number;
-  subTitle: string;
   supplierId: string;
-  supplierShopName: string;
   title: string;
   unit: string;
   productInfoExtDTO: productInfoExtDTOInfo;
   productAttributeDTOs: productAttributeDTOs;
 };
-
-export type HomeQueryType = Location['query'] & { userToken?: string; origin?: string };
